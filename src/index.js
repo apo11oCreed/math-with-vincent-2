fb0e1834cdc6d9903b61b36a61bcfef7465f9ed7
apo11oCreed <nelson.ncdesigns@gmail.com>
2019-08-18 00:39:12 -0400

    Adding React Bootstrap components and Jquery peer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './custom.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
